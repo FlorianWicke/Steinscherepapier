bb41e97c0f368b6a785fb79dba14135d20471ec7
Florian Wicke <flo@cookandcode.org>
2024-11-05 21:25:51 +0100

    Unentschieden eingebaut

--- code.js
+++ code.js
@@ -2,19 +2,37 @@
 const tools = ["rock", "paper","scissors"]
 
 
+let humanScore = 0
+let computerScore = 0
+
 function getComputerChoice(){
   let randomNumber = Math.random()
 
   if (randomNumber <0.334){
-    console.log(tools[0])
+    return (tools[0])
   } else if (randomNumber > 0.334 && randomNumber < 0.667){
-    console.log(tools[1])
+    return(tools[1])
   } else {
-    console.log(tools[2])
+    return(tools[2])
   }
-
-
 
 }
 
-getComputerChoice()
+function getHumanChoice(){
+  return prompt("What dou you take rock, paper, scissors?")
+
+}
+
+function playRound(){
+  let computerChoice = getComputerChoice()
+  console.log(computerChoice)
+
+  let humanChoice = getHumanChoice()
+
+  if (computerChoice === humanChoice){
+    console.log("Unentschieden, keiner einen Punkt")
+  }
+
+}
+
+playRound()
