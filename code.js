104c16c50d7945b3df7de434515ce90ea4892751
Florian Wicke <flo@cookandcode.org>
2024-11-05 21:48:50 +0100

    Spiel rudementär fertig gestellt

--- code.js
+++ code.js
@@ -5,6 +5,7 @@ const tools = ["rock", "paper","scissors"]
 let humanScore = 0
 let computerScore = 0
 
+
 function getComputerChoice(){
   let randomNumber = Math.random()
 
@@ -20,19 +21,58 @@ function getComputerChoice(){
 
 function getHumanChoice(){
   return prompt("What dou you take rock, paper, scissors?")
-
 }
 
 function playRound(){
   let computerChoice = getComputerChoice()
-  console.log(computerChoice)
-
   let humanChoice = getHumanChoice()
 
   if (computerChoice === humanChoice){
     console.log("Unentschieden, keiner einen Punkt")
   }
 
+  //Computer hat Stein, Spieler hat Paper -> Spieler gewinnt
+  if (computerChoice === "rock" && humanChoice === "paper"){
+    console.log("Spieler gewinnt")
+    humanScore += 1
+  }
+
+  //Computer hat Stein, Spieler hat Schere -> Computer gewinnt
+
+  if (computerChoice === "rock" && humanChoice === "scissors"){
+    console.log("Computer gewinnt")
+    computerScore += 1
+  }
+  //Computer hat Paper, Spieler hat Schere -> Spieler gewinnt
+  if (computerChoice === "paper" && humanChoice === "scissors"){
+    console.log("Spieler gewinnt")
+    humanScore += 1
+  }
+
+  //Computer hat Paper, Spieler hat Stein -> Computer gewinnt
+  if (computerChoice === "paper" && humanChoice === "rock"){
+    console.log("Computer gewinnt")
+    computerScore += 1
+  }
+  //Computer hat Schere, Spieler hat Papier -> Computer gewinnt
+  if (computerChoice === "scissors" && humanChoice === "paper"){
+    console.log("Computer gewinnt")
+    computerScore += 1
+  }
+
+  //Computer hat Schere, Spieler hat Stein -> Spieler gewinnt
+  if (computerChoice === "scissors" && humanChoice === "rock"){
+    console.log("Spieler gewinnt")
+    humanScore += 1
+  }
+
 }
 
-playRound()
+console.log("human " + humanScore)
+console.log("computer " + computerScore)
+
+while (humanScore < 3 && computerScore < 3){
+  playRound()
+  console.log("human " + humanScore)
+console.log("computer " + computerScore)
+}
