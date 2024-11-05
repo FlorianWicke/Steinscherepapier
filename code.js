c3417b3f871f584dc371dbda9a2b5d764978d41f
Florian Wicke <flo@cookandcode.org>
2024-11-05 21:11:11 +0100

    Buildiung ComputerChoise

--- code.js
+++ code.js
@@ -1,1 +1,20 @@
-console.log("Hello World")
+
+const tools = ["rock", "paper","scissors"]
+
+
+function getComputerChoice(){
+  let randomNumber = Math.random()
+
+  if (randomNumber <0.334){
+    console.log(tools[0])
+  } else if (randomNumber > 0.334 && randomNumber < 0.667){
+    console.log(tools[1])
+  } else {
+    console.log(tools[2])
+  }
+
+
+
+}
+
+getComputerChoice()
